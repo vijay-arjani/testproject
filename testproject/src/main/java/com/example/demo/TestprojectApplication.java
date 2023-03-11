76cc206bf9c96af5e2752e01989161d8c744c2e3
vijay-arjani <vijayarjani2@gmail.com>
2023-03-11 18:01:21 +0530

    First Commit

---
+++ testproject/src/main/java/com/example/demo/TestprojectApplication.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestprojectApplication.class, args);
+		
+		System.out.println("Hello World This is Test Project ");
+	}
+
+}
