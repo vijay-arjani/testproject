cdc947683fd4d41df8dc6f800f9cbdde756bcb6f
vijay-arjani <vijayarjani2@gmail.com>
2023-03-11 19:34:45 +0530

    Creating Student object using Lombook

--- testproject/src/main/java/com/example/demo/TestprojectApplication.java
+++ testproject/src/main/java/com/example/demo/TestprojectApplication.java
@@ -3,13 +3,23 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import pojo.Student;
+
 @SpringBootApplication
 public class TestprojectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TestprojectApplication.class, args);
 		
-		System.out.println("Hello World This is Test Project ");
+		Student student =new Student();
+		student.setId(1);
+		student.setName("Vijay");
+		student.setContact_number("8302210140");
+		
+		System.out.println("Student Details"+student.toString());
+		
+		
+		
 	}
 
 }
---
+++ testproject/src/main/java/pojo/Student.java
@@ -0,0 +1,11 @@
+package pojo;
+
+import lombok.Data;
+
+@Data
+public class Student {
+	private int id;
+	private String name;
+	private String contact_number;
+
+}
